27a567cfcbeab70db021bd2eda1ba03a1c0e93d8
BoolJob <javier.parra89@gmail.com>
2017-12-27 18:13:55 -0300

    commit javier

---
+++ insertausuario.sql
@@ -0,0 +1,31 @@
+-- Function: insertausuario(character varying, character varying, character varying, character varying, bigint, bigint, bigint, bigint, character)
+
+-- DROP FUNCTION insertausuario(character varying, character varying, character varying, character varying, bigint, bigint, bigint, bigint, character);
+
+CREATE OR REPLACE FUNCTION insertausuario(
+    _nombre character varying,
+    _apellido character varying,
+    _email character varying,
+    _rut character varying,
+    _pais bigint,
+    _region bigint,
+    _ciudad bigint,
+    _comuna bigint,
+    _password character)
+  RETURNS void AS
+$BODY$
+
+DECLARE _fecharegistro timestamp;
+
+BEGIN
+	_fecharegistro := (SELECT (now() AT TIME ZONE 'utc'));
+	
+	INSERT INTO usuario (nombre,apellido,email,rut,id_pais,id_region,id_ciudad,id_comuna,password,fecha_registro)
+	VALUES (_nombre,_apellido,_email,_rut,_pais,_region,_ciudad,_comuna,_password,_fecharegistro);
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION insertausuario(character varying, character varying, character varying, character varying, bigint, bigint, bigint, bigint, character)
+  OWNER TO postgres;
+
