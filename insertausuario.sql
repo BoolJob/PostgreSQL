a89bab26e2e57019da0382cd3e820ca8f0a7daf0
BoolJob <javier.parra89@gmail.com>
2017-12-28 18:30:08 -0300

    commit javier

--- insertausuario.sql
+++ insertausuario.sql
@@ -20,8 +20,8 @@ DECLARE _fecharegistro timestamp;
 BEGIN
 	_fecharegistro := (SELECT (now() AT TIME ZONE 'utc'));
 	
-	INSERT INTO usuario (nombre,apellido,email,rut,id_pais,id_region,id_ciudad,id_comuna,password,fecha_registro)
-	VALUES (_nombre,_apellido,_email,_rut,_pais,_region,_ciudad,_comuna,_password,_fecharegistro);
+	INSERT INTO usuario (nombre,apellido,email,rut,id_pais,id_region,id_ciudad,id_comuna,password,fecha_registro, estado)
+	VALUES (_nombre,_apellido,_email,_rut,_pais,_region,_ciudad,_comuna,_password,_fecharegistro, 0);
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
